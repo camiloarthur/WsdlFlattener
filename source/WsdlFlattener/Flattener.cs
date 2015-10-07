24f2592e9d1949409aea240c90bbc79d1febd6c2
Camilo Arthur <camilo.arthur@gmail.com>
2015-10-07 06:49:58 -0300

    Initial checkin
    console application to flatten wsdl in order to have a single file. It
    was created so we can use the wsdl to generate classes on SalesForce.com
    projects

---
+++ source/WsdlFlattener/Flattener.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using ServiceDescription = System.Web.Services.Description.ServiceDescription;
+
+namespace WsdlFlattener
+{
+    public class Flattener
+    {
+        public string GetFlattenedWsdl(string wsdlUrl, string filePathAndName)
+        {
+            var metadata = GetServiceMetadata(wsdlUrl, MetadataExchangeClientMode.HttpGet);
+            var newWsdl = GetFlattenedServiceDescription(metadata);
+
+            if (String.IsNullOrEmpty(filePathAndName))
+                filePathAndName = Path.GetTempFileName();
+
+            var tempPath = filePathAndName;
+            newWsdl.Write(tempPath);
+
+            string xml;
+
+            using (var sr = new StreamReader(tempPath))
+            {
+                xml = sr.ReadToEnd();
+            }
+
+            if(File.Exists(tempPath))
+                File.Delete(tempPath);
+
+            return xml;
+        }
+
+        private MetadataSet GetServiceMetadata(string wsdlUrl, MetadataExchangeClientMode clientMode)
+        {
+            Binding mexBinding;
+
+            if (clientMode == MetadataExchangeClientMode.HttpGet)
+                mexBinding = new BasicHttpBinding { MaxReceivedMessageSize = 50000000L };
+            else
+                mexBinding = new WSHttpBinding(SecurityMode.None) { MaxReceivedMessageSize = 50000000L };
+
+            var mexClient = new MetadataExchangeClient(mexBinding)
+            {
+                ResolveMetadataReferences = true,
+                MaximumResolvedReferences = 200
+            };
+
+            return mexClient.GetMetadata(new Uri(wsdlUrl), clientMode);
+        }
+
+        public ServiceDescription GetFlattenedServiceDescription(MetadataSet metadataSet)
+        {
+            var obj = Assembly.GetAssembly(typeof(WsdlExporter)).GetType("System.ServiceModel.Description.WsdlHelper", true)
+                .GetMethod("GetSingleWsdl", BindingFlags.Static | BindingFlags.Public)
+                .Invoke(null, new object[1] { metadataSet });
+
+            if (obj is ServiceDescription)
+                return (ServiceDescription)obj;
+
+            return null;
+        }
+    }
+}
---
+++ source/WsdlFlattenerConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using WsdlFlattener;
+
+namespace WsdlFlattenerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var flat = new Flattener();
+            string url;
+            var filePath = string.Empty;
+            var tempPath = Path.GetTempFileName();
+
+            if (args.Length >= 2)
+            {
+                url = args[0];
+                filePath = args[1];
+            }
+            else if (args.Length == 1)
+            {
+                url = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Usage: wf [wsdlUrl] [outputFile]");
+                return;
+            }
+            
+            var xml = flat.GetFlattenedWsdl(url, tempPath);
+
+            if (String.IsNullOrWhiteSpace(filePath))
+            {
+                Console.WriteLine(xml);
+            }
+            else
+            {
+                if(File.Exists(filePath))
+                    File.Delete(filePath);
+
+                using (var sw = File.CreateText(filePath))
+                {
+                    sw.Write(xml);
+                }
+
+                Console.WriteLine(@"File ""{0}"" successfully created", filePath);
+            }
+        }
+    }
+}
